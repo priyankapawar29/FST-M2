4745f0f1334bedc6e9ec6b23711625f63cba8309
Priyanka Pawar <49469215+priyankapawar29@users.noreply.github.com>
2021-05-25 18:05:22 +0530

    Update ProjectTestClass.java

--- api-project/ProjectTestClass.java
+++ api-project/ProjectTestClass.java
@@ -12,14 +12,14 @@ public class ProjectTestClass {
 
 	// Declare request specification
 	RequestSpecification requestSpec;
-	String ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCuEj94g7d3JNFc6VYHKVvJg0ric2Ub9cvhQ7yoHYo2P+og+PuBJqqLxDvur07TByRbWC/BCAYjAC2wQf6fD92ZF1ZBHUw7NV5aAKcEu2C90ow0JuQDiNLyvt0d+0CCCnyVBQkj1IwY5V9BwUC9Jl7tZ0UlTwmiu4u6QZC8ZC3OhTbP0uR5dYSzcHDt20oUZ0Fo+7XaxW/u6X193gp8s8pJpr3O5rVulnx5E3lqvQdDbvT6HNqQ0ZFVNcaXesKVTkRMF7KcjgFjAC/hZjC7328+URqQsmSqmaw6H0mrhcrkpn3lt+8/tkObv8SHKyJDxx1c+F2kDWp0wjbRxUJyZsZ92xkNaovkCNQOXxz4z/a3tz/U3xJvMCzhFKs1ybSuFhZCj3GIPaN384DgWf/zqMY2wuKpG67zyygkNq32DX93vbH4Ts9igsD6+GB8Svt7kSlpZd5rWth9i7lqbUehjQo0uWop6KQZmP3G7PidakHLpg+IBF1ffD4xpe13x3/XY+k=";
+	String ssh_key = "XXXX";
 	int ssh_id;
 
 	@BeforeClass
 	public void setUp() {
 		// Create request specification
 		requestSpec = new RequestSpecBuilder().setContentType(ContentType.JSON)
-				.addHeader("Authorization", "token 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")
+				.addHeader("Authorization", "token XXXX")
 				.setBaseUri("https://api.github.com/").build();
 	}
 
